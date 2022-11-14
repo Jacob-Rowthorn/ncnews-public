f327d7594e509ca2a52497934a078ff43cb59022
Jacob-Rowthorn <jacob.rowthorn1@nhs.net>
2022-11-14 11:31:42 +0000

    Exported from index.jses

--- db/data/test-data/index.js
+++ db/data/test-data/index.js
@@ -2,3 +2,5 @@ exports.articleData = require('./articles.js');
 exports.commentData = require('./comments.js');
 exports.topicData = require('./topics.js');
 exports.userData = require('./users.js');
+
+export const fullArticle = {articleData, commentData, topicData, userData}
